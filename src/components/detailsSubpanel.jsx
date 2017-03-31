6888e2504f72b5719e53e98a525c789ac9a25077
aSqrd-eSqrd <austin.albright@gmail.com>
2017-03-31 17:09:29 -0400

    Added a click handler so the expand-collapse chevron will change on the detailsSubpanel component. Also, useful when embedding inside another JS framework (e.g. AngularJS). The expand-collapse click kept getting hijacked.

--- src/components/detailsSubpanel.jsx
+++ src/components/detailsSubpanel.jsx
@@ -3,13 +3,24 @@
 import React from 'react';
 
 class NodeDetailsSubpanel extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      expanded: this.props.expanded ? this.props.expanded : false
+    };
+  }
+
+  changeState (newState) {
+    this.setState(newState);
+  }
+
   render () {
     const badge = this.props.badge;
     const title = this.props.title.replace(/\s/g, '_');
     const headingId = `${title}Heading`;
     const collapseId = `collapse${title}`;
 
-    const expanded = this.props.expanded;
+    const expanded = this.state.expanded;
     const iconClass = `glyphicon ${expanded ? 'glyphicon-chevron-down' : 'glyphicon-chevron-right'}`;
     const iconStyle = {
       fontSize: '12px',
@@ -20,7 +31,7 @@ class NodeDetailsSubpanel extends React.Component {
         <div className="panel panel-default">
           <div className="panel-heading" role="tab" id={headingId}>
             <h4 className="panel-title">
-              <a role="button" data-toggle="collapse" href={`#${collapseId}`} aria-controls={collapseId} className={`accordion-toggle${expanded ? ' collapsed' : ''}`}>
+              <a role="button" data-toggle="collapse" href={`#${collapseId}`} aria-controls={collapseId} className={`accordion-toggle${expanded ? '' : ' collapsed'}`} onClick={() => this.changeState({ expanded: !expanded })}>
                 <span className={iconClass} style={iconStyle}></span> {this.props.title.toUpperCase()} {badge ? <span className="badge">{badge}</span> : undefined}
               </a>
             </h4>
